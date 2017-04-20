03357f26de20a945d1330fc7ae77a4b9102adede
Sean Reigle <sereigle@gmail.com>
2017-04-20 11:38:02 -0700

    Updated server.js comments

--- lynn-sean/server.js
+++ lynn-sean/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
 app.get('/new', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // Request '/' is #2 of our diagram requesting from the server.js file and the response from new.html is #5 of our diagram. This would be the 'read' portion of the "CRUD". The line of code is handling a request/response for the new.html
+  // Request is #2 of our diagram requesting from the server.js file and the response from new.html is #5 of our diagram. The method used in article.js is .loadAll and .fetchAll. This would be the 'read' portion of the "CRUD". The line of code is handling a request/response for the new.html
   response.sendFile('new.html', {root: './public'});
 });
 
@@ -24,7 +24,7 @@ app.get('/new', function(request, response) {
 // REVIEW: Routes for making API calls to use CRUD Operations on our database
 app.get('/articles', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // Request '/new' is #2 and the response new.html is #5 of our diagram. We believe this would be 'read' part of the "CRUD". The line of code is handling a request/response for the new.html
+  // An AJAX request for articles DB to the server was from Article.fetchAll(), then the server forms that request into a SQL query to the database and returns to the user a response containing the results of the request. This is a CRUD "Read" operation that goes through numbers #2, #3, #4, and #5 in the drawing.
   client.query('SELECT * FROM articles')
   .then(function(result) {
     response.send(result.rows);
@@ -36,7 +36,7 @@ app.get('/articles', function(request, response) {
 
 app.post('/articles', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // TThis line of code is using #1,#2,#3,#4 & #5 from our diagram. It is interacting with the Article.prototype.insertRecord method in the js file. It is the 'Create' portion in the "CRUD" It is getting a request from the client and processing that request and adding it to the database and giving a response of "insert complete" or giving an error message.
+  // It is interacting with the Article.prototype.insertRecord method in the article.js file. It is using the 'Create' portion in "CRUD" It is getting a request from the client and processing that request and adding it to the database and giving a response of "insert complete" or giving an error message to the user. This line of code is using #1,#2,#3,#4 and #5 from our diagram.
   client.query()
     `INSERT INTO
     articles(title, author, "authorUrl", category, "publishedOn", body)
